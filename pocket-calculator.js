8e6f6d95687c07b8a69a2afcdf324029ebb7b75c
Aaronk2711 <32553805+Aaronk2711@users.noreply.github.com>
2019-01-07 11:02:50 -0500

    Update pocket-calculator.js

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -4,113 +4,71 @@ var answer;
 do {
 
 function button1() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
   document.getElementById("result").textContent += "1";
   answer = document.getElementById("result").innerHTML;
 }
 
 function button2() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
   document.getElementById("result").textContent += "2";
   answer = document.getElementById("result").innerHTML;
 }
 
 function button3() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
   document.getElementById("result").textContent += "3";
   answer = document.getElementById("result").innerHTML;
 }
 
 function button4() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
   document.getElementById("result").textContent += "4";
   answer = document.getElementById("result").innerHTML;
 }
 
 function button5() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
   document.getElementById("result").textContent += "5";
   answer = document.getElementById("result").innerHTML;
 }
 
 function button6() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
   document.getElementById("result").textContent += "6";
   answer = document.getElementById("result").innerHTML;
 }
 
 function button7() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
   document.getElementById("result").textContent += "7";
   answer = document.getElementById("result").innerHTML;
 }
 
 function button8() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
   document.getElementById("result").textContent += "8";
   answer = document.getElementById("result").innerHTML;
 }
 
 function button9() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
   document.getElementById("result").textContent += "9";
   answer = document.getElementById("result").innerHTML;
 }
 
 function button0() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
   document.getElementById("result").textContent += "0";
   answer = document.getElementById("result").innerHTML;
 }
 
 function add() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
   document.getElementById("result").textContent += "+";
   answer = document.getElementById("result").innerHTML;
 }
 
 function subtract() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
   document.getElementById("result").textContent  += "-";
   answer = document.getElementById("result").innerHTML;
 }
 
 function multiply() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
   document.getElementById("result").textContent += "*";
   answer = document.getElementById("result").innerHTML;
 }
 
 function divide() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
   document.getElementById("result").textContent += "/";
   answer = document.getElementById("result").innerHTML;
 }
