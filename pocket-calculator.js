d3ffeb2528b32045a0bd88059b03d73b88459816
Aaron Kulkarni <akulkarni@ucvts.org>
2019-01-08 13:20:14 -0500

    Added decimal, fixed percentage, fixed deletion problem:

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -4,125 +4,134 @@ var answer;
 do {
 
 function button1() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
   document.getElementById("result").textContent += "1";
   answer = document.getElementById("result").innerHTML;
 }
 
 function button2() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
   document.getElementById("result").textContent += "2";
   answer = document.getElementById("result").innerHTML;
 }
 
 function button3() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
   document.getElementById("result").textContent += "3";
   answer = document.getElementById("result").innerHTML;
 }
 
 function button4() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
   document.getElementById("result").textContent += "4";
   answer = document.getElementById("result").innerHTML;
 }
 
 function button5() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
   document.getElementById("result").textContent += "5";
   answer = document.getElementById("result").innerHTML;
 }
 
 function button6() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
   document.getElementById("result").textContent += "6";
   answer = document.getElementById("result").innerHTML;
 }
 
 function button7() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
   document.getElementById("result").textContent += "7";
   answer = document.getElementById("result").innerHTML;
 }
 
 function button8() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
   document.getElementById("result").textContent += "8";
   answer = document.getElementById("result").innerHTML;
 }
 
 function button9() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
   document.getElementById("result").textContent += "9";
   answer = document.getElementById("result").innerHTML;
 }
 
 function button0() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
   document.getElementById("result").textContent += "0";
   answer = document.getElementById("result").innerHTML;
 }
 
 function add() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
   document.getElementById("result").textContent += "+";
   answer = document.getElementById("result").innerHTML;
 }
 
 function subtract() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
   document.getElementById("result").textContent  += "-";
   answer = document.getElementById("result").innerHTML;
 }
 
 function multiply() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
   document.getElementById("result").textContent += "*";
   answer = document.getElementById("result").innerHTML;
 }
 
 function divide() {
-  if (document.getElementById("result").innerHTML = "Sorry, that is not a valid operation.") {
-    clean();
-  }
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
   document.getElementById("result").textContent += "/";
   answer = document.getElementById("result").innerHTML;
 }
 
+function decimal() {
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
+  document.getElementById("result").textContent += ".";
+  answer = document.getElementById("result").innerHTML;
+}
+
 function clean() {
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
   document.getElementById("result").innerHTML = "";
   answer = document.getElementById("result").innerHTML;
 }
 
 function negate() {
-  answer = answer * -1;
-  if ((Number.isNaN(answer)) || (answer === "Infinity" || (answer = "/0") || (answer[1,2] = "**") || (answer[1,2] = "++") || (answer[1,2] == "--"))){
+  answer = document.getElementById("result").innerHTML;
+  answer = eval(answer) * -1;
+  if ((Number.isNaN(answer)) || (answer === "Infinity" || (answer === "/0") || (answer[1,2] === "**") || (answer[1,2] === "++") || (answer[1,2] === "--"))){
+    document.getElementById("result").innerHTML = "Sorry, that is not a valid operation";
+  }
+
+  else {
+  document.getElementById("result").innerHTML = eval(answer);
+  }
+}
+
+function percentage() {
+  answer = document.getElementById("result").innerHTML;
+  answer = eval(answer);
+  answer = answer / 100;
+  if ((Number.isNaN(answer)) || (answer === "Infinity" || (answer === "/0") || (answer[1,2] === "**") || (answer[1,2] === "++") || (answer[1,2] === "--"))){
     document.getElementById("result").innerHTML = "Sorry, that is not a valid operation";
   }
 
@@ -133,8 +142,7 @@ function negate() {
 
   function equals() {
     answer = document.getElementById("result").innerHTML;
-
-    if ((Number.isNaN(answer)) || (answer === "Infinity" || (answer = "/0") || (answer = "**") || (answer = "++") || (answer[1,2] == "--"))){
+    if ((Number.isNaN(answer)) || (answer === "Infinity" || (answer === "/0") || (answer === "**") || (answer === "++") || (answer[1,2] === "--"))){
       document.getElementById("result").innerHTML = "Sorry, that is not a valid operation"
     }
 
