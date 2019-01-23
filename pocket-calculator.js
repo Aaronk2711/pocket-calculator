e2a86c5aa7eb866771cf5b923201e509f9455ace
Aaron Kulkarni <akulkarni@ucvts.org>
2019-01-22 23:41:45 -0500

    done 15 19 minutes early

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -4,160 +4,141 @@ var total = "0";
 
 do {
 
-function button1() {
+function buttonnumber(number) {
+  number = number.toString();
   display = display.toString();
   if (document.getElementById("display").innerHTML === "0") {
-    wipe();
+    zeroWipe();
   }
-  display = display.toString();
-  document.getElementById("display").textContent += "1";
-  display = document.getElementById("display").innerHTML;
-}
-
-function button2() {
-  display = display.toString();
-  if (document.getElementById("display").innerHTML === "0") {
-    wipe();
+  if (display.indexOf("e") > -1) {
+    display = Number(display);
+    display = display.toString();
+    display += number;
+    display = Number(display);
+    display = display.toExponential(5);
+    document.getElementById("display").innerHTML = display;
   }
+  else {
   display = display.toString();
-  document.getElementById("display").textContent += "2";
+  document.getElementById("display").textContent += number;
   display = document.getElementById("display").innerHTML;
-}
 
-function button3() {
-  display = display.toString();
-  if (document.getElementById("display").innerHTML === "0") {
-    wipe();
-  }
-  display = display.toString();
-  document.getElementById("display").textContent += "3";
-  display = document.getElementById("display").innerHTML;
-}
-
-function button4() {
-  if (document.getElementById("display").innerHTML === "0") {
-    wipe();
-  }
-  display = display.toString();
-  document.getElementById("display").textContent += "4";
-  display = document.getElementById("display").innerHTML;
-}
-
-function button5() {
-  if (document.getElementById("display").innerHTML === "0") {
-    wipe();
-  }
-  display = display.toString();
-  document.getElementById("display").textContent += "5";
-  display = document.getElementById("display").innerHTML;
-}
-
-function button6() {
-  if (document.getElementById("display").innerHTML === "0") {
-    wipe();
-  }
-  display = display.toString();
-  document.getElementById("display").textContent += "6";
-  display = document.getElementById("display").innerHTML;
-}
-
-function button7() {
-  if (document.getElementById("display").innerHTML === "0") {
-    wipe();
-  }
-  display = display.toString();
-  document.getElementById("display").textContent += "7";
-  display = document.getElementById("display").innerHTML;
-}
-
-function button8() {
-  if (document.getElementById("display").innerHTML === "0") {
-    wipe();
-  }
-  display = display.toString();
-  document.getElementById("display").textContent += "8";
-  display = document.getElementById("display").innerHTML;
-}
-
-function button9() {
-  if (document.getElementById("display").innerHTML === "0") {
-    wipe();
-  }
-  display = display.toString();
-  document.getElementById("display").textContent += "9";
-  display = document.getElementById("display").innerHTML;
-}
-
-function button0() {
-  if (document.getElementById("display").innerHTML === "0") {
-    wipe();
-  }
-  display = display.toString();
-  document.getElementById("display").textContent += "0";
-  display = document.getElementById("display").innerHTML;
-}
-
-function add() {
-    if (document.getElementById('total').innerHTML === "0") {
-      extraWipe();
+    if (display.length > 9) {
+    display = Number(display);
+    display = display.toExponential(5);
+    display = display.toString();
     }
+  document.getElementById("display").innerHTML = display;
+  }
+}
+
+/*function add() {
+    if (document.getElementById('total').innerHTML === "0") {
+      totalWipe();
+    }
+    if ((total.charAt(total.length-1) === "-") ||(total.charAt(total.length-1) === "*") || (total.charAt(total.length-1) === "/")) {
+      total = document.getElementById("total").innerHTML;
+      total = total.toString();
+      total = total.replace(total.charAt(total.length-1), "+")
+      document.getElementById("total").innerHTML = total;
+      zeroWipe();
+    }
+    else {
     total = document.getElementById("total").innerHTML;
     total = total.toString();
     total += display;
     total += "+";
     document.getElementById("total").innerHTML = total;
-    wipe();
+    zeroWipe();
+  }
   }
 
-function subtract() {
-    if (document.getElementById("total").innerHTML === "0") {
-       extraWipe();
-    }
-       total = document.getElementById("total").innerHTML;
-    total = total.toString();
-    total += display;
-    total += "-";
-    document.getElementById("total").innerHTML = total;
-    wipe();
-  }
+  function subtract() {
+      if (document.getElementById('total').innerHTML === "0") {
+        totalWipe();
+      }
+      total = document.getElementById("total").innerHTML;
+      total = total.toString();
+      total += display;
+      total += "-";
+      document.getElementById("total").innerHTML = total;
+      zeroWipe();
+      display = document.getElementById("display").innerHTML;
+      }
 
-function multiply() {
-if (document.getElementById('total').innerHTML === "0") {
-      extraWipe();
-    }
-    total = document.getElementById("total").innerHTML;
-    total = total.toString();
-    total += display;
-    total += "*";
-    document.getElementById("total").innerHTML = total;
-    wipe();
-  }
+    function multiply() {
+        if (document.getElementById('total').innerHTML === "0") {
+          totalWipe();
+        }
+        total = document.getElementById("total").innerHTML;
+        total = total.toString();
+        total += display;
+        total += "*";
+        document.getElementById("total").innerHTML = total;
+        zeroWipe();
+      }
 
-function divide() {
-if (document.getElementById('total').innerHTML === "0") {
-      extraWipe();
-    }
-    total = document.getElementById("total").innerHTML;
-    total = total.toString();
-    total += display;
-    total += "/";
-    document.getElementById("total").innerHTML = total;
-    wipe();
-  }
+      function divide() {
+          if (document.getElementById('total').innerHTML === "0") {
+            totalWipe();
+          }
+        total = document.getElementById("total").innerHTML;
+          total = total.toString();
+          total += display;
+          total += "/";
+          document.getElementById("total").innerHTML = total;
+          zeroWipe();
+      }
+      */
+
+      function operator(name) {
+          if (document.getElementById('total').innerHTML === "0") {
+            totalWipe();
+          }
+          total += display;
+          if ((total.charAt(total.length-2) === "-") ||(total.charAt(total.length-2) === "*") || (total.charAt(total.length-2) === "/") || (total.charAt(total.length-1) === "+")) {
+            total = document.getElementById("total").innerHTML;
+            total = total.toString();
+            total = total.replace(total.charAt(total.length-2), name)
+            document.getElementById("total").innerHTML = total;
+            zeroWipe();
+          }
+          else {
+          total = document.getElementById("total").innerHTML;
+          total = total.toString();
+          total += display;
+          total += name;
+          document.getElementById("total").innerHTML = total;
+          zeroWipe();
+        }
+        }
 
 function decimal() {
   display = document.getElementById("display").innerHTML;
   display = display.toString();
+  if (display.indexOf(".") > -1) {
+
+  }
+  else {
   document.getElementById("display").textContent += ".";
   display = document.getElementById("display").innerHTML;
+}
 }
 
 function wipe() {
   document.getElementById("display").innerHTML = "0";
 }
 
-function extraWipe() {
+function totalWipe() {
   document.getElementById("total").innerHTML = "";
+}
+
+function zeroWipe() {
+  document.getElementById("display").innerHTML = "";
+}
+
+function totalZeroWipe() {
+  document.getElementById("total").innerHTML = "0"
 }
 
 function clean() {
@@ -166,13 +147,15 @@ function clean() {
 
 function extraClean() {
   document.getElementById("display").innerHTML = "0";
+  display = "0";
   document.getElementById("total").innerHTML = "0";
+  total = "0";
 }
 
 function negate() {
   display = document.getElementById("display").innerHTML;
   display = eval(display) * -1;
-  if ((Number.isNaN(display)) || (display === "Infinity" || (display === "/0") || (display[1,2] === "**") || (display[1,2] === "++") || (display[1,2] === "--"))){
+    if ((Number.isNaN(eval(display))) || (eval(display) === "Infinity") || (total.indexOf("/0") > -1) || (total.indexOf("**") > -1) || (total.indexOf("++") > -1)){
     document.getElementById("display").innerHTML = "Sorry, that is not a valid operation";
   }
 
@@ -183,38 +166,78 @@ function negate() {
 
 function percentage() {
   display = document.getElementById("display").innerHTML;
-  display = eval(display);
-  display = display / 100;
-  if ((Number.isNaN(display)) || (display === "Infinity" || (display === "/0") || (display[1,2] === "**") || (display[1,2] === "++") || (display[1,2] === "--"))){
+  if ((Number.isNaN(eval(display))) || (eval(display) === "Infinity") || (total.indexOf("/0") > -1) || (total.indexOf("**") > -1) || (total.indexOf("++") > -1)){
     document.getElementById("display").innerHTML = "Sorry, that is not a valid operation";
   }
-
   else {
+    if (total.indexOf("--") > -1) {
+      total.replace("--", "+");
+    }
+  display = eval(display);
+  display = display / 100
   document.getElementById("display").innerHTML = eval(display);
   }
 }
 
   function equals() {
+    display = document.getElementById("display").innerHTML;
+    console.log(display, "first display");
     total = document.getElementById("total").innerHTML;
-    total = total.toString();
+    if (total === "0") {
+      totalWipe();
+    }
+    total = document.getElementById("total").innerHTML;
+    console.log(total, "first total")
     total += display;
+    console.log(total, "second total");
     document.getElementById("total").innerHTML = total;
-    if ((Number.isNaN(total)) || (eval(total) === null) || (eval(total) === undefined) || (eval(total) === "Infinity")){
+      if ((Number.isNaN(eval(display))) || (eval(display) === "Infinity") || (total.indexOf("/0") > -1) || (total.indexOf("**") > -1) || (total.indexOf("++") > -1)){
       document.getElementById("display").innerHTML = "Sorry, that is not a valid operation"
     }
+    else {
     if (total.indexOf("--") > -1) {
       total.replace("--", "+");
     }
-    document.getElementById("display").innerHTML = eval(total);
 
+    if (display.indexOf("--") > -1) {
+      display.replace("--", "+");
+    }
+
+    if (total.indexOf(",") > -1) {
+      total.replace(",", "");
+    }
+
+    if (display.indexOf(",") > -1) {
+      display.replace(",", "");
+    }
+
+
+    if (total.toString() === "0") {
+      document.getElementById("display").innerHTML = eval(display);
+    }
+    else {
+    total = document.getElementById("total").innerHTML;
+    display = eval(total);
+    display = display.toString();
+    if (display.length > 9) {
+      display = Number(display);
+      display = display.toExponential();
+      document.getElementById("display").innerHTML = display;
+      console.log("it get's here");
+    }
+    else {
+    document.getElementById("display").innerHTML = eval(total).toLocaleString();
+    display = document.getElementById("display").innerHTML;
+    totalZeroWipe();
+    console.log(display, "second display");
+    console.log("ez clappers");
+    }
+  }
+}
 
 
     completed = true;
   }
 
-
-/*function pi() {
-  document.getElementById("display").textContent += "π";
-}*/
 
 } while(completed = false)
